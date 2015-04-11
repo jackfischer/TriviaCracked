b4dfb8197e21d7b41bb4ea0b91be6a231f8ec99b
Jack Fischer <jfische8@binghamton.edu>
2015-04-11 13:38:03 -0400

    moving towards a nice json to serve

--- inline.py
+++ inline.py
@@ -1,9 +1,19 @@
+
+#runs integrated with mitmdump via the -s flag.
+#parses around all the crap traffic for whats going to preguntados.
+
 import requests
 
 def response(context, flow):
   #print "Inside python"
   #if flow.request.pretty_host(hostheader=True).contains("api.preguntados.com"):
+
+  #ignore requests to analytics services and native background stuff
   if ("api.preguntados.com" in flow.request.pretty_host(hostheader=True)):
-    r = requests.post("http://localhost:5000", data=flow.response.content)
-    print flow.response.content
+    print flow.response.content #see everything going to preguntados
+    #ignore requests for profile data, etc
+    if ("answer" in flow.request.path or "games" in flow.request.path):       
+      #post request to LOCAL flask listener.
+      r = requests.post("http://localhost:5000", data=flow.response.content)
+
 
--- listener.py
+++ listener.py
@@ -7,22 +7,48 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def hello():
-  print "\nInside flask, got Post... "
+  print "\n\nInside flask, got Post... "
   data = json.loads(request.data)
-  #question = data["spins_data"]["spins"][0]["questions"][0]["question"]["correct_answer"]
-  question = data["spins_data"]["spins"][0]["questions"][0]["question"]
-  answerIndex = question["correct_answer"]
-  answerContent = question["answers"][int(answerIndex)]
+
+  #check if crown case
+  #qType = "NORMAL"
+  #if (data["spins_data"]["spins"][0]["type"] is "CROWN"):
+  #  qType = "CROWN"
+
+  #narrow json down to just the questions core
+  questions = data["spins_data"]["spins"][0]["questions"]
+
+  #clean up powerups, extra baggage
+  for i in range (0,5):
+    del questions[i]["powerup_question"]
+    del questions[i]["question"]["author"]
+    del questions[i]["question"]["id"]
+    del questions[i]["question"]["media_type"]
+
+  #add the nice "type" flag for chris
+  #api["type"] = qType
+  #api["questions"] = questions
+
+  print(json.dumps(questions))
+
+
+  ###################
+  #Old model, works
+
+  #question = data["spins_data"]["spins"][0]["questions"][0]["question"]
+  #answerIndex = question["correct_answer"]
+  #answerContent = question["answers"][int(answerIndex)]
+
+  #print("Correct answer's index: ")
+  #print(answerIndex)
+  #print(answerContent)
+  #return request.data
 
   #pp = pprint.PrettyPrinter(indent=4)
   #pp.pprint(answerIndex)
+  ###################
 
-  print("Correct answer's index: ")
-  print(answerIndex)
-  print(answerContent)
-  #print "Answer index: " + answerIndex
-  #print "CORRECT ANSWER: " + question["answers"][answerIndex]
-  return request.data
+
 
 if __name__ == "__main__":
   app.run()
