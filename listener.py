edb2538b5301e065b6dca2d51828a64f9cffca01
Jack Fischer <jfische8@binghamton.edu>
2015-04-12 00:08:38 -0400

    great detective work by metamind guy, chris, and joe

--- listener.py
+++ listener.py
@@ -19,53 +19,48 @@ def answer():
 @app.route("/", methods=['GET', 'POST'])
 def hello():
   global globalAnswers
-  print "\n\nInside flask, got Post... "
   data = json.loads(request.data)
 
-  #check if crown case
-#  qType = "NORMAL"
-#  if (data["spins_data"]["spins"][0]["type"] is "CROWN"):
-#    qType = "CROWN"
-
   print data
-  #narrow json down to just the questions core
-  questions = data["spins_data"]
-  questions = questions["spins"]
-  questions = questions[0]["questions"]
+  if "spins_data" in data:
+    print "has attr!!"
+    #narrow json down to just the questions core
+    questions = data["spins_data"]
+    questions = questions["spins"]
+    questions = questions[0]["questions"]
 
-  #clean up powerups, extra baggage
-  for i in range (len(questions)):
-    del questions[i]["powerup_question"]
-    del questions[i]["question"]["author"]
-    del questions[i]["question"]["id"]
-    del questions[i]["question"]["media_type"]
-
-#  print questions
-  #add the nice "type" flag for chris
- # api["type"] = qType
- # api["questions"] = questions
-
-  #globalAnswers = json.dumps(questions)
-  globalAnswers = json.dumps(questions)
-  print(json.dumps(questions))
-  return json.dumps(questions)
+    #clean up powerups, extra baggage
+    for i in range (len(questions)):
+      del questions[i]["powerup_question"]
+      del questions[i]["question"]["author"]
+      del questions[i]["question"]["id"]
+      del questions[i]["question"]["media_type"]
 
 
-  ###################
-  #Old model, works
+    #globalAnswers = json.dumps(questions)
+    globalAnswers = json.dumps(questions)
+    print(json.dumps(questions))
+    return json.dumps(questions)
 
-  #question = data["spins_data"]["spins"][0]["questions"][0]["question"]
-  #answerIndex = question["correct_answer"]
-  #answerContent = question["answers"][int(answerIndex)]
 
-  #print("Correct answer's index: ")
-  #print(answerIndex)
-  #print(answerContent)
-  #return request.data
+    ###################
+    #Old model, works
 
-  #pp = pprint.PrettyPrinter(indent=4)
-  #pp.pprint(answerIndex)
-  ###################
+    #question = data["spins_data"]["spins"][0]["questions"][0]["question"]
+    #answerIndex = question["correct_answer"]
+    #answerContent = question["answers"][int(answerIndex)]
+
+    #print("Correct answer's index: ")
+    #print(answerIndex)
+    #print(answerContent)
+    #return request.data
+
+    #pp = pprint.PrettyPrinter(indent=4)
+    #pp.pprint(answerIndex)
+    ###################
+  else:
+    print "DOES NOT have attr"
+    return globalAnswers
 
 
 
